80d847f2987a7866a1d92b3dd692376aab8a3429
mgordo <m.gordo8@gmail.com>
2016-05-10 19:29:41 +0200

    plots working, small bug that makes everything 0 still exists

--- pyStatistics/plotter.py
+++ pyStatistics/plotter.py
@@ -8,7 +8,7 @@ import os
 
 '''Change and add to the list as needed. Should contain path to archives'''
 auxroute='C:/maven/'
-archives = [auxroute+'/logs/xxx.log', auxroute+'/logs/yyy.log']
+archives = [auxroute+'plots/ttl2.log', auxroute+'plots/ttl5.log']
 data=[]
 for ele in archives:
     total_sent = 0
@@ -18,11 +18,15 @@ for ele in archives:
         if "Final log" in line:
             parts = line.split(": ")
             total_sent+=int(parts[-1])
+            
             received.append(int(parts[1].split(",")[0]))
+            
 
     
-    print "Total sent file "+ele+": "+total_sent
-    percentages = [x/total_sent for x in received]
+    print "Total sent file "+ele+": "+str(total_sent)
+    
+    percentages = [float(x/total_sent) for x in received]
+
     data.append(percentages)
     f.close()
     
@@ -30,4 +34,5 @@ plt.boxplot(data)
 plt.ylabel("Percentages")
 
 routefig=os.path.normpath(auxroute+'plots/boxplots.png')
-plt.savefig()
+plt.savefig(routefig,dpi=300,bbox_inches='tight')
+print "Fininshed succesfully!"
